5f778e4e8e0ba501a716b10a2c9e11078e826a1c
Vigneswaran Shanmugathas <vigneswaran.shanmugathas@flowbird.group>
2022-08-14 14:10:58 +0200

    feat(pages/2_water.py): icon

--- pages/2_water.py
+++ pages/2_water.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 st.set_page_config(
-    page_title="Water", page_icon="🌍",
+    page_title="Water", page_icon="🌊",
     layout="wide",
     initial_sidebar_state="expanded",
 )
